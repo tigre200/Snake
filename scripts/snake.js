ac5b77bf05cc60c42cfc55f17c88d609fb3635b1
Hugo Costa <hugosequeiracosta@gmail.com>
2016-05-12 18:14:37 +0100

    Moved drawing functions to separate folder called drawing.js

--- scripts/snake.js
+++ scripts/snake.js
@@ -149,7 +149,7 @@ var keyState = {
 	}
 };
 //Função que inicializa os objetos para começar o jogo
-function init() {
+function start() {
 	score = 0;
 	gameOver = false;
 	grid.init(EMPTY, COLUMNS, ROWS);
@@ -220,45 +220,4 @@ function update() {
 		//Coloca a nova cabeça na cobra, criando a ilusão de movimento
 		snake.insert(hx,hy);
 	}
-}
-//Função que desenha no canvas o jogo
-function drawCanvas(canvas /*canvas onde se desenha o jogo*/,
-			   ctx /*contexto do canvas para desenhar*/,
-			   emptyColor,
-			   snakeColor,
-			   fruitColor,
-			   scoreColor,
-			   scoreXPos,
-			   scoreYPos) {
-		//Largura da célula no canvas
-	var cellWidth = canvas.width/grid.width,
-		//Altura da célula no canvas
-		cellHeight = canvas.height/grid.height;
-	//Para cada célula desenha um retângulo com uma cor diferente dependente do seu valor
-	for (var x=0; x<grid.width; x++) {
-		for (var y=0; y<grid.height; y++) {
-			switch (grid.get(x,y)) {
-				case EMPTY:
-					ctx.fillStyle = emptyColor;
-					break;
-				case SNAKE:
-					ctx.fillStyle = snakeColor;
-					break;
-				case FRUIT:
-					ctx.fillStyle = fruitColor;
-					break;
-			}
-			ctx.fillRect(x*cellWidth, y*cellHeight,cellWidth,cellHeight);
-		}
-	}
-	//Desenha a pontuação no canto inferior esquerdo do canvas
-	ctx.fillStyle = scoreColor;
-	ctx.fillText("Score: " + score, scoreXPos, scoreYPos);
-}
-//Função que termina o jogo, mostrando a pontuação obtida
-function end(snakeScore) {
-	//Parágrafo que mostra a pontuação
-	var endp = document.createElement("p");
-	endp.innerHTML = "Game Over<br/>Your score is:<br/>" + snakeScore;
-	document.body.appendChild(endp);
 }
